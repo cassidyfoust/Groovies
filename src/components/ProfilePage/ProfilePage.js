c0d1b96ac24426e9756378638dd278dcfd2a74de
Cassidy Foust <cassidy.foust@gmail.com>
2019-10-15 15:55:50 -0500

    added cards for preferences

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './ProfilePage.css';
+import { styled } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { flexbox } from '@material-ui/system';
+import AddIcon from '@material-ui/icons/Add';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+const MyCard = styled(Card)({
+    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // color: 'white',
+    height: 32,
+    width: 90,
+    paddingLeft: 7,
+    paddingTop: 5,
+    margin: 5,
+    fontSize: 12,
+    display: flexbox,
+    textAlign: "center"
+});
+
+const MyDelete = styled(DeleteIcon)({
+    padding: 0,
+    margin: 0,
+    height: 30,
+})
+
+const NewIconBtn = styled(IconButton)({
+    padding: 0,
+    margin: 0
+})
 
 const ProfilePage = () => (
     <div>
@@ -11,8 +40,59 @@ const ProfilePage = () => (
             Your Preferences:
     </h1>
     <p>You Like:</p>
+        <div className="prefs">
+        <MyCard>
+            Genre One
+                <NewIconBtn>
+                    <MyDelete/>
+                </NewIconBtn>
+        </MyCard>
+        <MyCard>
+            Genre Two
+                <NewIconBtn>
+                    <MyDelete />
+                </NewIconBtn>
+        </MyCard>
+            <MyCard>
+                Genre Three
+                <NewIconBtn>
+                    <MyDelete />
+                </NewIconBtn>
+            </MyCard>
+            <MyCard>
+                <NewIconBtn>
+                    <AddIcon />
+                </NewIconBtn>
+            </MyCard>
+        </div>
     <p>You Dislike:</p>
-    <p>You've Watched:</p>
+        <div className="prefs">
+            <MyCard>
+                Genre One
+                <NewIconBtn>
+                    <MyDelete />
+                </NewIconBtn>
+            </MyCard>
+            <MyCard>
+                Genre Two
+                <NewIconBtn>
+                    <MyDelete/>
+                </NewIconBtn>
+            </MyCard>
+            <MyCard>
+                Genre Three
+                <NewIconBtn>
+                    <MyDelete />
+                </NewIconBtn>
+            </MyCard>
+            <MyCard>
+                <NewIconBtn>
+                    <AddIcon/>
+                </NewIconBtn>
+            </MyCard>
+        </div>
+    <p>You Watched:</p>
+    Someday there will be a carousel here
     </div>
 );
 
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = [{
+    entry: './app.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'style-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules', 'node_modules/@material/*']
+                                    .map((d) => path.join(__dirname, d))
+                                    .map((g) => glob.sync(g))
+                                    .reduce((a, c) => a.concat(c), [])
+                            }
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+}];
