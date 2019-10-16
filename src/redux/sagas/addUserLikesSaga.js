0cf894e6dc5434bf7496d248dd1094358100d2d0
Cassidy Foust <cassidy.foust@gmail.com>
2019-10-16 16:30:57 -0500

    added saga to post likes and dislikes

---
+++ src/redux/sagas/addUserLikesSaga.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { put } from 'redux-saga/effects';
+
+function* addUserLikes(action) {
+    try {
+        const response = yield axios.post(`/api/add_user_likes`, action.payload)
+        console.log('response:', response)
+        yield put({ type: 'FETCH_USER_GENRES', payload: action.payload.user_id });
+    } catch (error) {
+        console.log('error while fetching user genres:', error)
+    }
+}
+
+export default addUserLikes;
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -5,6 +5,8 @@ import userSaga from './userSaga';
 import getGroups from './groupsSaga';
 import getUserGenres from './userGenresSaga';
 import deleteUserGenre from './deleteUserGenreSaga';
+import addUserLikes from './addUserLikesSaga';
+import addUserDislikes from './addUserDislikesSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -17,6 +19,8 @@ export default function* rootSaga() {
   yield takeEvery ('FETCH_GROUPS', getGroups);
   yield takeEvery ('FETCH_USER_GENRES', getUserGenres);
   yield takeEvery ('DELETE_USER_GENRE', deleteUserGenre);
+  yield takeEvery ('ADD_USER_LIKES', addUserLikes);
+  yield takeEvery ('ADD_USER_DISLIKES', addUserDislikes)
   yield all([
     loginSaga(),
     registrationSaga(),
