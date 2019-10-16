1936eb1de54a3f736b18ae4f7a547d6ce067cf2e
Cassidy Foust <cassidy.foust@gmail.com>
2019-10-16 13:31:04 -0500

    added query to get user groups

---
+++ server/routes/groups.router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/:id', (req, res) => {
+    const queryText = `SELECT "group_name" FROM "user"
+        JOIN "user_group" ON "user".id = "user_group".user_id
+        JOIN "group" ON "group".id = "user_group".group_id
+        WHERE "user".id=$1;`
+    pool.query(queryText, [req.params.id])
+        .then((result) => { res.send(result.rows); })
+        .catch((err) => {
+            console.log('Error completing USER GROUPS query', err);
+            res.sendStatus(500);
+        });
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
--- src/redux/sagas/groupsSaga.js
+++ src/redux/sagas/groupsSaga.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { put } from 'redux-saga/effects';
 
-function* getGroups() {
+function* getGroups(action) {
     try {
-        const response = yield axios.get('/api/groups');
+        const response = yield axios.get(`/api/groups/${action.payload}`);
         console.log('response:', response)
         yield put({ type: 'SET_USER_GROUPS', payload: response.data })
     } catch (error) {
-        console.log('error while fetching genres:', error)
+        console.log('error while fetching groups:', error)
     }
 }
 
